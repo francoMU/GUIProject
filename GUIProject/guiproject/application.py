93f79928a46899730c3cdbd238109edf9d9f4816
Franco Moitzi <franco.moitzi@stud.unileoben.ac.at>
2021-05-19 19:16:43 -0400

    feature(project): seperate dialog

--- GUIProject/guiproject/application.py
+++ GUIProject/guiproject/application.py
@@ -4,8 +4,9 @@ import pkg_resources
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (QAction, QApplication, QDesktopWidget, QDialog, QFileDialog,
-                             QHBoxLayout, QLabel, QMainWindow, QToolBar, QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QAction, QApplication, QDesktopWidget, QFileDialog,
+                             QHBoxLayout, QMainWindow, QToolBar, QWidget)
+from guiproject.dialogs import AboutDialog
 
 
 class Template(QMainWindow):
@@ -82,38 +83,6 @@ class Template(QMainWindow):
         if accepted:
             with open(filename) as file:
                 file.read()
-
-
-class AboutDialog(QDialog):
-    """Create the necessary elements to show helpful text in a dialog."""
-
-    def __init__(self, parent=None):
-        """Display a dialog that shows application information."""
-        super(AboutDialog, self).__init__(parent)
-
-        self.setWindowTitle('About')
-        help_icon = pkg_resources.resource_filename('guiproject.images',
-                                                    'ic_help_black_48dp_1x.png')
-        self.setWindowIcon(QIcon(help_icon))
-        self.resize(300, 200)
-
-        author = QLabel('Franco')
-        author.setAlignment(Qt.AlignCenter)
-
-        icons = QLabel('Material design icons created by Google')
-        icons.setAlignment(Qt.AlignCenter)
-
-        github = QLabel('GitHub: francoMU')
-        github.setAlignment(Qt.AlignCenter)
-
-        self.layout = QVBoxLayout()
-        self.layout.setAlignment(Qt.AlignVCenter)
-
-        self.layout.addWidget(author)
-        self.layout.addWidget(icons)
-        self.layout.addWidget(github)
-
-        self.setLayout(self.layout)
 
 
 def main():
